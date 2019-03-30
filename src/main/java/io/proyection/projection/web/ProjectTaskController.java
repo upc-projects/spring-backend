5236c753bfd822a67a69934dee2ac3f4f7c9d36e
diegosalas27 <dominicsc2hs@gmail.com>
2019-03-30 16:52:28 -0500

    Se ha creado el crud para projectTask

---
+++ src/main/java/io/proyection/projection/web/ProjectTaskController.java
@@ -0,0 +1,59 @@
+package io.proyection.projection.web;
+
+import io.proyection.projection.domain.ProjectTask;
+import io.proyection.projection.service.ProjectTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/board")
+@CrossOrigin
+public class ProjectTaskController {
+
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @PostMapping("")
+    public ResponseEntity<?> addPTToBoard(@Valid @RequestBody ProjectTask projectTask, BindingResult result) {
+
+        if(result.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+
+            for(FieldError error: result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
+        ProjectTask newPT = projectTaskService.save(projectTask);
+
+        return new ResponseEntity<ProjectTask>(newPT, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all")
+    public Iterable<ProjectTask> getAllPTs() {
+        return projectTaskService.findAll();
+    }
+
+    @GetMapping("/{pt_id}")
+    public ResponseEntity<?> getPTById(@PathVariable Long pt_id){
+        ProjectTask projectTask = projectTaskService.findById((pt_id));
+        return new ResponseEntity<ProjectTask>(projectTask, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{pt_id}")
+    public ResponseEntity<?> deleteProjectTask(@PathVariable Long pt_id){
+        projectTaskService.delete(pt_id);
+
+        return new ResponseEntity<String>("Project Task deleted", HttpStatus.OK);
+    }
+}

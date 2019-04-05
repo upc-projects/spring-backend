c245ed8b5e99612c99f17976efd59b9ec1087679
RodrigoAGM <rod-gm@hotmail.com>
2019-04-05 01:46:11 -0500

    Change task status function added to service

--- src/main/java/io/proyection/projection/service/ProjectTaskService.java
+++ src/main/java/io/proyection/projection/service/ProjectTaskService.java
@@ -37,4 +37,20 @@ public class ProjectTaskService implements IProjectTaskService{
         ProjectTask projectTask = findById(id);
         projectTaskRepository.delete(projectTask);
     }
+
+    public ProjectTask changeStatus(Long id_task, int new_status){
+
+        ProjectTask projectTask = projectTaskRepository.getById(id_task);
+        int status;
+
+        if(new_status == StatusType.TO_DO.getNumVal()){
+            status = StatusType.TO_DO.getNumVal();
+        }else if(new_status == StatusType.DOING.getNumVal()){
+            status = StatusType.DOING.getNumVal();
+        }else{
+            status = StatusType.DONE.getNumVal();
+        }
+        projectTask.setStatus(status);
+        return projectTaskRepository.save(projectTask);
+    }
 }

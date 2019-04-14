4c27c42b53ec23a49b5add92cf4dd88128a39ca0
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-13 21:15:51 -0500

    add get response entity toa void duplicated code

--- src/main/java/io/proyection/projection/ProjectionApplication.java
+++ src/main/java/io/proyection/projection/ProjectionApplication.java
@@ -1,17 +1,10 @@
 package io.proyection.projection;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjectionApplication {
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectionApplication.class, args);

4f6688057120c5df0c4515d09cbf4d59d47e0eaf
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-13 21:16:11 -0500

    Reformat new controllers

--- src/main/java/io/proyection/projection/web/UserController.java
+++ src/main/java/io/proyection/projection/web/UserController.java
@@ -2,16 +2,14 @@ package io.proyection.projection.web;
 
 import io.proyection.projection.domain.User;
 import io.proyection.projection.service.IUserService;
+import io.proyection.projection.utils.Commons;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -24,15 +22,8 @@ public class UserController {
     @PostMapping("")
     public ResponseEntity<?> addUser(@Valid @RequestBody User user, BindingResult result) {
 
-        if(result.hasErrors()){
-            Map<String, String> errorMap = new HashMap<>();
-
-            for(FieldError error: result.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = Commons.getResponseEntity(result);
+        if (errorMap != null) return errorMap;
 
         User newUser = userService.save(user);
 

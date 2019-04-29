be8dc8a218f2bd5ea7580bd0f440c961eec07a29
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-29 00:49:34 -0500

    update repositories

--- src/main/java/io/proyection/projection/repository/TaskRepository.java
+++ src/main/java/io/proyection/projection/repository/TaskRepository.java
@@ -1,11 +1,20 @@
 package io.proyection.projection.repository;
 
 import io.proyection.projection.domain.Task;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface TaskRepository extends CrudRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 
     Task getById(Long id);
+
+    Page<Task> findByUserId(Long userId, Pageable pageable);
+
+    Optional<Task> findByIdAndUserId(Long id, Long userId);
+
 }

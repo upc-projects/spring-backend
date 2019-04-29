59caa09cc0fb314cb52ff422340b2e8359393fd2
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-29 00:47:58 -0500

    enable jpa auditing

--- src/main/java/io/proyection/projection/ProjectionApplication.java
+++ src/main/java/io/proyection/projection/ProjectionApplication.java
@@ -2,8 +2,10 @@ package io.proyection.projection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ProjectionApplication {
 
     public static void main(String[] args) {
